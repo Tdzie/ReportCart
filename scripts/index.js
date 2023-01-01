a0b824479628b7d4473abcd158cb42b812ad712c
Tdzie <65301716+Tdzie@users.noreply.github.com>
2023-01-01 13:38:46 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -307,10 +307,20 @@ function runner(a) {
     
 
                 // insert useful information
-               // var kk = document.createElement("a");
+               var kk = document.createElement("div");
+               Kk.innerHTML += "<div class="container text-center">
+  <div class="row row-cols-3">
+    <div class="col">Column 1</div>
+    <div class="col">Column 2</div>
+    <div class="col">Column 3</div>
+    <div class="col">Column 4</div>
+    <div class="col">Column 5</div>
+    <div class="col">Column 6</div>
+  </div>
+</div>";
                // kk.setAttribute("id", "aImage");
                 // kk.innerHTML += '<img src="images/KeyInHelper.jpg">';
-               // add.appendChild(kk);
+               add.appendChild(kk);
                
             }
 
